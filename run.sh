85f383f0436d04c87f4358c50826fed95c51cca2
Derkai <2363104914@qq.com>
2024-11-19 18:41:26 +0800

    [style]: 删除不必要的支持文件.

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ echo "[START] Open-Vins "
 # 通过本脚本文件路径来获取 x152b 项目文件根目录
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
 
-gnome-terminal -- bash -c "source ${PROJECT_DIR}/devel/setup.bash;roslaunch ov_msckf subscribe_d430.launch"  && sleep 1;
+source ${PROJECT_DIR}/devel/setup.bash;roslaunch ov_msckf subscribe_d430.launch  && sleep 1;
